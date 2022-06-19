5f15af829a0a61aafc73980f03b93639b82dac4d
dreamxinxcode <brandon@dreamincode.dev>
2022-06-19 12:46:48 -0700

    Add meals cards and categories

--- src/app/meals-tab/meals-tab.page.ts
+++ src/app/meals-tab/meals-tab.page.ts
@@ -7,9 +7,163 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MealsTabPage implements OnInit {
 
+  meals: any;
+
   constructor() { }
 
   ngOnInit() {
+    this.meals = {
+      breakfast: [
+        {
+          title: 'Title',
+          image: '',
+          tags: ['Low Carb', 'Berries'],
+          views: 0,
+          timestamp: new Date(),
+        },        
+        {
+          title: 'Title',
+          image: '',
+          tags: [],
+          views: 0,
+          timestamp: new Date(),
+        },        
+        {
+          title: 'Title',
+          image: '',
+          tags: [],
+          views: 0,
+          timestamp: new Date(),
+        },        
+        {
+          title: 'Title',
+          image: '',
+          tags: [],
+          views: 0,
+          timestamp: new Date(),
+        },
+      ],
+      lunch: [
+        {
+          title: 'Title',
+          image: '',
+          tags: [],
+          views: 0,
+          timestamp: new Date(),
+        },        
+        {
+          title: 'Title',
+          image: '',
+          tags: [],
+          views: 0,
+          timestamp: new Date(),
+        },        
+        {
+          title: 'Title',
+          image: '',
+          tags: [],
+          views: 0,
+          timestamp: new Date(),
+        },        
+        {
+          title: 'Title',
+          image: '',
+          tags: [],
+          views: 0,
+          timestamp: new Date(),
+        },
+      ],
+      dinner: [
+        {
+          title: 'Title',
+          image: '',
+          tags: [],
+          views: 0,
+          timestamp: new Date(),
+        },        
+        {
+          title: 'Title',
+          image: '',
+          tags: [],
+          views: 0,
+          timestamp: new Date(),
+        },        
+        {
+          title: 'Title',
+          image: '',
+          tags: [],
+          views: 0,
+          timestamp: new Date(),
+        },        
+        {
+          title: 'Title',
+          image: '',
+          tags: [],
+          views: 0,
+          timestamp: new Date(),
+        },
+      ],
+      snacks: [
+        {
+          title: 'Title',
+          image: '',
+          tags: [],
+          views: 0,
+          timestamp: new Date(),
+        },        
+        {
+          title: 'Title',
+          image: '',
+          tags: [],
+          views: 0,
+          timestamp: new Date(),
+        },        
+        {
+          title: 'Title',
+          image: '',
+          tags: [],
+          views: 0,
+          timestamp: new Date(),
+        },        
+        {
+          title: 'Title',
+          image: '',
+          tags: [],
+          views: 0,
+          timestamp: new Date(),
+        },
+      ],
+      dessert: [
+        {
+          title: 'Title',
+          image: '',
+          tags: [],
+          views: 0,
+          timestamp: new Date(),
+        },        
+        {
+          title: 'Title',
+          image: '',
+          tags: [],
+          views: 0,
+          timestamp: new Date(),
+        },        
+        {
+          title: 'Title',
+          image: '',
+          tags: [],
+          views: 0,
+          timestamp: new Date(),
+        },        
+        {
+          title: 'Title',
+          image: '',
+          tags: [],
+          views: 0,
+          timestamp: new Date(),
+        },
+      ],
+    };
   }
 
 }
