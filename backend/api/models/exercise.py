9f726a8c0ab34c60f3311b6fb67dd5a0cc336053
dreamxinxcode <dreamxinxcode@gmail.com>
2023-03-10 11:05:06 +0000

    Add exercise create serializer

--- backend/api/models/exercise.py
+++ backend/api/models/exercise.py
@@ -1,8 +1,10 @@
 from django.db import models
-
+from users.models import CustomUser
 
 class Exercise(models.Model):
   name = models.CharField(max_length=50, unique=True)
+  image = models.ImageField(blank=True, null=True)
+  user = models.ForeignKey(CustomUser, blank=True, null=True, on_delete=models.CASCADE)
 
   def __str__(self):
       return self.name
